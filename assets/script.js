53c69c5162d7c2340e455aca60b557dadce4d6d4
LE Palmer <lpalmer@ucsd.edu>
2024-04-11 11:20:35 -0700

    added weather icons and functionality according to weather condition, render button bug fixed, added fonts, css changes

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,19 @@ var apiKey = "69dacd3e106811916a8d25c1fbe9dc73"; //my API key
 var now = dayjs().format("MM/DD/YYYY");
 
 const searchInput = document.getElementById("search-input");
+const forecastContainer = document.getElementById("forecast");
 var cityList = document.getElementById("city-list");
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+/*
+    imports
+
+    global scope stuff
+
+    functions
+
+    exports
+*/
 
 // function to push city name to search history array in local storage
 searchInput.addEventListener("keypress", function (e) {
@@ -12,12 +24,13 @@ searchInput.addEventListener("keypress", function (e) {
 });
 
 function search() {
+  console.log("Searching...");
   var city = searchInput.value;
-  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
   //   check if city is already in search history
   if (!searchHistory.includes(city)) {
     searchHistory.push(city);
+    console.log("City pushed:", city, searchHistory);
     localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
     getLatLon(city);
     renderButtons();
@@ -39,6 +52,7 @@ function getLatLon(city) {
       if (!response.ok) {
         throw new Error("City not found");
       }
+      forecastContainer.style.visibility = "visible";
       return response.json();
     })
     .then((data) => {
@@ -65,9 +79,51 @@ function getCurrent(lat, lon) {
     .catch((error) => console.error("Error:", error));
 }
 
+function getWeatherIcon(condition, now, sunrise, sunset) {
+  now /= 1000;
+  // console.log(`
+  //   condition: ${condition},
+  //   now: ${now},
+  //   sunrise: ${sunrise},
+  //   sunset: ${sunset},
+  // `)
+  switch (condition) {
+    case "Clear":
+      return "./assets/images/Clear.png";
+    case "Clouds":
+      if (now > sunrise && now < sunset) {
+        return "./assets/images/Clouds.png";
+      } else {
+        return "./assets/images/Night.png";
+      }
+    case "Rain":
+      return "./assets/images/Rain.png";
+    case "Snow":
+      return "./assets/images/Snow.png";
+  }
+}
+
 // function to display city weather data on screen
 function displayWeatherdata(data) {
-  // console.log(data);
+  var condition = data.list[0].weather[0].main;
+  var sunrise = data.city.sunrise;
+  var sunset = data.city.sunset;
+  var iconUrl = getWeatherIcon(
+    condition,
+    dayjs(now).valueOf(),
+    sunrise,
+    sunset
+  );
+  console.log(
+    `
+    Getting current icon
+      condition: ${condition}
+      sunrise: ${sunrise}
+      sunset: ${sunset}
+    icon: ${iconUrl}
+  `,
+    data
+  );
 
   // create elements to display the current weather data
   // data object contains the current weather information accessible for the city
@@ -84,6 +140,7 @@ function displayWeatherdata(data) {
 
   var city = document.createElement("h1");
   city.textContent = data.city.name + " " + now;
+  city.style.fontFamily ='Montserrat, sans-serif';
   weatherDataDiv.appendChild(city);
 
   weatherDataDiv.textContent = data.name;
@@ -91,32 +148,39 @@ function displayWeatherdata(data) {
   weatherDataDiv.style.color = "white";
 
   var iconImg = document.createElement("img");
-  iconImg.src = `https://openweathermap.org/img/w/${icon}.png`;
+  var condition = data.list[0].weather[0].main;
+  // iconImg.src = `https://openweathermap.org/img/w/${icon}.png`;
+  iconImg.src = iconUrl;
   iconImg.className = "getCurrent-icon";
   weatherDataDiv.appendChild(iconImg);
 
   var weatherDiv = document.createElement("div");
   weatherDiv.className = "weather-description";
   weatherDiv.textContent = `Weather Description: ${weatherdata}`;
+  weatherDiv.style.fontFamily ='Open Sans, sans-serif';
   weatherDataDiv.appendChild(weatherDiv);
 
   var temperatureCard = document.createElement("div");
   temperatureCard.className = "temperature";
+  temperatureCard.style.fontFamily ='Open Sans, sans-serif';
   temperatureCard.textContent = `Temperature: ${temperature}` + " F";
   weatherDataDiv.appendChild(temperatureCard);
 
   var feels_likeDiv = document.createElement("div");
   feels_likeDiv.className = "feels-like";
+  feels_likeDiv.style.fontFamily ='Open Sans, sans-serif';
   feels_likeDiv.textContent = `Feels Like: ${feelslike}` + " F";
   weatherDataDiv.appendChild(feels_likeDiv);
 
   var humidityDiv = document.createElement("div");
   humidityDiv.className = "humidity";
+  humidityDiv.style.fontFamily='Open Sans, sans-serif';
   humidityDiv.textContent = `Humidity: ${humidity}`;
   weatherDataDiv.appendChild(humidityDiv);
 
   var windSpeedDiv = document.createElement("div");
   windSpeedDiv.className = "wind-speed";
+  windSpeedDiv.style.fontFamily='Open Sans, sans-serif';
   windSpeedDiv.textContent = `Wind Speed: ${windSpeed}`;
   weatherDataDiv.appendChild(windSpeedDiv);
 }
@@ -135,6 +199,7 @@ function getForecast(lat, lon) {
       var fivedayDiv = document.createElement("h4");
       fivedayDiv.textContent = "5 Day Forecast";
       fivedayDiv.classList = "five-day-title";
+      fivedayDiv.style.fontFamily='Montserrat, sans-serif';
       forecastDiv.appendChild(fivedayDiv);
 
       var rowDiv = document.createElement("div");
@@ -151,26 +216,38 @@ function getForecast(lat, lon) {
 
         var dateDiv = document.createElement("h5");
         dateDiv.textContent = new Date(dayData.dt_txt).toLocaleDateString();
+        dateDiv.style.fontFamily='Open Sans, sans-serif'
+        dateDiv.style.fontWeight='bold';
         dateDiv.className = "card-date";
         cardDiv.appendChild(dateDiv);
 
         var icon = document.createElement("img");
-        icon.src = `https://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
+        // icon.src = `https://openweathermap.org/img/w/${dayData.weather[0].icon}.png`;
+        var condition = dayData.weather[0].main;
+        icon.src = getWeatherIcon(
+          condition,
+          new Date().getTime(),
+          data.city.sunrise,
+          data.city.sunset
+        );
         icon.width = 100;
         icon.height = 100;
         icon.className = "icon-img";
-
+        icon.style.marginBottom = "40px";
         cardDiv.appendChild(icon);
 
         var temperatureDiv = document.createElement("div");
         temperatureDiv.textContent = `Temperature: ${dayData.main.temp}`;
+        temperatureDiv.style.fontFamily='Open Sans, sans-serif';
         cardDiv.appendChild(temperatureDiv);
 
         var humidityDiv = document.createElement("div");
+        humidityDiv.style.fontFamily='Open Sans, sans-serif';
         humidityDiv.textContent = `Humidity: ${dayData.main.humidity}`;
         cardDiv.appendChild(humidityDiv);
 
         var windSpeedDiv = document.createElement("div");
+        windSpeedDiv.style.fontFamily='Open Sans, sans-serif';
         windSpeedDiv.textContent = `Wind Speed: ${dayData.wind.speed}`;
         cardDiv.appendChild(windSpeedDiv);
 
@@ -181,18 +258,19 @@ function getForecast(lat, lon) {
 }
 
 // get previous city search history from local storage
-var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
 renderButtons();
 
 function renderButtons() {
   cityList.innerHTML = "";
+  console.log("rendering buttons:", searchHistory);
   searchHistory.forEach(renderButton);
 }
 
 // create buttons for each city, assign value and display on screen
 function renderButton(city) {
   var cityListItem = document.createElement("button");
-  cityListItem.className = "list-group-item list-group-item-action";
+  cityListItem.className = "btn-primary";
   cityListItem.textContent = city;
 
   // Add event listener to the button
